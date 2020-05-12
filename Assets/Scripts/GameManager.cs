baece32fd7685948d2de03880f3e7d71bb48fc06
jonatas57 <jdjonatasduarte57@gmail.com>
2020-05-12 16:34:42 -0300

    Limita o movimento do jogador

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TileType {
+  FREE,
+  HOLE,
+  HYDRANT
+}
+
 public class GameManager : MonoBehaviour {
 
   private static GameManager instance = null;
   private Vector3 xAxis;
   private Vector3 yAxis;
   public GameObject playerPrefab;
+  public GameObject holePrefab;
+
+  private TileType[][] board;
+  private int boardSize;
 
   public static GameManager Instance {
     get {
@@ -31,10 +41,32 @@ public class GameManager : MonoBehaviour {
     Vector3 size = new Vector3(21, 21, 0);
     xAxis = size.x * Vector3.right;
     yAxis = size.y * Vector3.up;
-    for(int i=0; i < 2; i++) {
-        GameObject playerObject = Instantiate<GameObject>(playerPrefab);
-        playerObject.GetComponent<PlayerController>().SetButtons(i);
-        playerObject.transform.position = GridToVectorPosition(1, 1);
+
+    boardSize = 12;
+    board = new TileType[boardSize + 2][];
+    for (int i = 0;i < boardSize + 2;i++) {
+      board[i] = new TileType[boardSize + 2];
+      for (int j = 0;j < boardSize + 2;j++) {
+        if (i == 0 || j == 0 || i == boardSize + 1 || j == boardSize + 1) {
+          board[i][j] = TileType.HOLE;
+        }
+        else board[i][j] = TileType.FREE;
+      }
+    }
+
+    for (int i = 0;i < boardSize + 2;i++) {
+      for (int j = 0;j < boardSize + 2;j++) {
+        if (board[i][j] == TileType.HOLE) {
+          GameObject hole = Instantiate<GameObject>(holePrefab);
+          holePrefab.transform.position = GridToVectorPosition(i, j);
+        }
+      }
+    }
+
+    for(int i=0; i < 1; i++) {
+      GameObject playerObject = Instantiate<GameObject>(playerPrefab);
+      playerObject.GetComponent<PlayerController>().SetButtons(i);
+      playerObject.transform.position = GridToVectorPosition(1, 1);
     }
   }
 
