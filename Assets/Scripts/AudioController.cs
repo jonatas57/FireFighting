bc3e320f7df2665bb06dc31452816bc22c9b09bd
jonatas57 <jdjonatasduarte57@gmail.com>
2020-06-16 19:29:48 -0300

    Adiciona música de fundo durante a partida

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+
+  public AudioSource audioSource;
+  public AudioClip introSound;
+  public AudioClip backgroundMusic;
+
+  private void Start() {
+    StartCoroutine(playMusic());
+  }
+
+  private IEnumerator playMusic() {
+    audioSource.clip = introSound;
+    audioSource.Play();
+    yield return new WaitForSeconds(introSound.length);
+
+    audioSource.clip = backgroundMusic;
+    audioSource.loop = true;
+    audioSource.Play();
+  }
+}
